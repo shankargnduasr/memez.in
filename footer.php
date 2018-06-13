41d75da8c09f68253ac7bfed0ab913aa52ce2563
shankar <shankargnduasr@gmail.com>
2018-06-14 01:29:32 +0530

    year changed
    year changed

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="container">
       <div class="row">
            <h4 class="text-center">
-	&copy;All copy rights are revised by memez.in 2017
+	&copy;All copy rights are revised by memez.in 2018
 	</h4>
    </div>
 </div>
